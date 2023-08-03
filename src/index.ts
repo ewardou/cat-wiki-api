498fa64233c2c8e58b679e7809cd9cf373b48920
Eduardo Sánchez <eduardosr1599@gmail.com>
2023-08-02 22:02:54 -0400

    Add cors package

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import fetch from 'node-fetch'
 import { config } from 'dotenv'
+import cors from 'cors'
 
 config()
 
@@ -16,6 +17,7 @@ interface API {
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.get('/breeds', (_req, res, next) => {
   fetch('https://api.thecatapi.com/v1/breeds')
